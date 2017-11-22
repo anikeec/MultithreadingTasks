3c434cf26cc8efe27fd01501ee757560a6bacbb8
apu <pasha_anik@ukr.net>
2017-11-22 15:49:48 +0200

    Make task WaitTutor

--- src/main/java/com/apu/multithreadingtasks/WaitTutor.java
+++ src/main/java/com/apu/multithreadingtasks/WaitTutor.java
@@ -5,6 +5,10 @@
  */
 package com.apu.multithreadingtasks;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Test;
 
 /**
@@ -25,8 +29,19 @@ public class WaitTutor {
 	Thread t1, t2;
 	Object monitor = new Object();
 	int runningThreadNumber = 1;
-	int t1Counter = 0, t2Counter = 0;
-	//int maxCounter = 0;
+	volatile int t1Counter = 0, t2Counter = 0;
+        final CyclicBarrier barrier = new CyclicBarrier(2, new AdditionalThread());
+        
+        class AdditionalThread implements Runnable {
+            
+            @Override
+            public void run() {                
+                if((t1Counter%10 == 0) && (t2Counter%10 == 0) && (t1Counter >= 10)) {
+                    System.out.println("Value = " + t1Counter);
+                }
+            }
+            
+        }
 
 	class TestThread implements Runnable {
 		String threadName;
@@ -40,10 +55,15 @@ public class WaitTutor {
 		@Override
 		public void run() {
 			for (int i=0;i<100;i++) {
+                                try {
+                                    barrier.await();
+                                } catch (InterruptedException | BrokenBarrierException ex) {
+                                    ex.printStackTrace();
+                                }
 				System.out.println(threadName+":"+i);
 				synchronized(monitor) {
 					if (n==1) t1Counter = i;
-					if (n==2) t2Counter = i;
+					if (n==2) t2Counter = i;   
 					monitor.notify();
 					Thread.yield();
 					try {
@@ -72,7 +92,7 @@ public class WaitTutor {
 	public void testThread() {
 		t1 = new Thread(new TestThread("t1", 1));
 		t2 = new Thread(new TestThread("t2", 2));
-	    System.out.println("Starting threads");
+                System.out.println("Starting threads");
 		t1.start();
 		t2.start();
 
@@ -84,4 +104,8 @@ public class WaitTutor {
 			e.printStackTrace();
 		}
 	}
+        
+        public static void main(String[] args) {
+            new WaitTutor().testThread();
+        }
 }
